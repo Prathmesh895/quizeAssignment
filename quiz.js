a3e9ba0aa448a477c7d2a14a99ebbbab69607071
Prathmesh895 <125139465+Prathmesh895@users.noreply.github.com>
2023-05-22 18:42:55 +0530

    s

---
+++ quiz.js
@@ -0,0 +1,9 @@
+
+function select_button1() {
+     //Move to another page
+   window.location.href = "Quize_Questions.html";
+}
+//var counter = 0;
+//function count() {
+ //   document.write(counter++);
+//}
